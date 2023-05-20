22b8d2fc5360faf9f2b27385cca0919144132cb0
Sunkyu Jang <38223044+sunkyuj@users.noreply.github.com>
2023-05-20 22:18:10 +0900

    Update 가장 큰 수.py

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -5,15 +5,6 @@ class Number:
     def __lt__(self, other):
         return self.string_n+other.string_n < other.string_n+self.string_n
 
-    def __le__(self, other):
-        return self.string_n+other.string_n <=other.string_n+ self.string_n
-
-    def __gt__(self, other):
-        return self.string_n+other.string_n > other.string_n+self.string_n
-
-    def __ge__(self, other):
-        return self.string_n+other.string_n <=other.string_n+ self.string_n
-
     def __eq__(self, other):
         return self.string_n+other.string_n ==other.string_n+ self.string_n
     
@@ -24,24 +15,5 @@ def solution(numbers):
     snums.sort(reverse=1)
     for snum in snums:
         answer += snum.string_n
-#     for snum in snums:
-#         d[snum[0]].append(Number(snum)) # mod, org
-        
-#     for i in range(9,-1,-1):
-#         d[str(i)].sort(key=lambda x:(-x[0],len))
-#         print(d[str(i)])
-#         for mod,org in d[str(i)]:
-#             answer += org
-    return answer
-
-"""
-
-35 34 3 3331 32 31 30
-
-67 6667 6 65 64 63
-
-
-"""
-
-
-
+    answer = answer.lstrip('0')
+    return answer if answer else '0'
