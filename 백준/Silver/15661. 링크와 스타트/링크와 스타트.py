7ea83814a9efa1b2272746e92744d1b2b60308b1
Sunkyu Jang <38223044+sunkyuj@users.noreply.github.com>
2023-11-03 01:33:25 +0900

    [Silver I] Title: 링크와 스타트, Time: 7940 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15661. 링크와 스타트/링크와 스타트.py
+++ 백준/Silver/15661. 링크와 스타트/링크와 스타트.py
@@ -2,6 +2,7 @@ from itertools import combinations
 
 n = int(input())
 board = [list(map(int, input().split())) for _ in range(n)]
+all = list(range(n))
 ans = 9999
 
 
@@ -15,11 +16,7 @@ def power(team):
 
 for r in range(1, n // 2 + 1):  # 10
     for t1 in combinations(range(n), r):
-        t2 = []
-        for i in range(n):
-            if i not in t1:
-                t2.append(i)
-
+        t2 = set(all) - set(t1)
         ans = min(ans, abs(power(t1) - power(t2)))
 
 print(ans)
