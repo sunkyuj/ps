10d4a172a1df05b1f1850752c8c4bd610cf402c2
Sunkyu Jang <38223044+sunkyuj@users.noreply.github.com>
2023-12-30 17:53:17 +0900

    [Gold IV] Title: 연구소 2, Time: 504 ms, Memory: 34112 KB -BaekjoonHub

---
+++ 백준/Gold/17141. 연구소 2/연구소 2.py
@@ -0,0 +1,49 @@
+from collections import deque
+from itertools import combinations
+
+MAX = 999999999
+
+n, m = map(int, input().split())
+dydx = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+in_range = lambda y, x: 0 <= y < n and 0 <= x < n
+board = [list(map(int, input().split())) for _ in range(n)]
+vir_able = []
+spaces = 0
+for i in range(n):
+    for j in range(n):
+        if board[i][j] == 2:
+            vir_able.append((i, j))
+        if board[i][j] != 1:
+            spaces += 1
+
+
+def bfs(vir_list):
+    q = deque()
+    visit = [[0] * n for _ in range(n)]
+    for i, j in vir_list:
+        visit[i][j] = 1
+        q.append((i, j, 0))
+    cnt = 0
+    while q:
+        y, x, time = q.popleft()
+        cnt += 1
+
+        for dy, dx in dydx:
+            ny, nx = y + dy, x + dx
+            if not in_range(ny, nx) or visit[ny][nx]:
+                continue
+            if board[ny][nx] != 1:
+                q.append((ny, nx, time + 1))
+                visit[ny][nx] = 1
+    if cnt == spaces:
+        return time
+    return MAX
+
+
+ans = MAX
+for comb in combinations(vir_able, m):
+    ans = min(ans, bfs(list(comb)))
+if ans == MAX:
+    print(-1)
+else:
+    print(ans)
